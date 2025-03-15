5b133783c8e6d808dc777a489f91b387c1fe41c5
Viraj Kothalawala <vj@heydoor.com.au>
2025-03-15 23:48:16 +1100

    Feat(vulnerability): add data synchronization module

--- sentinelprobe/api/router.py
+++ sentinelprobe/api/router.py
@@ -5,6 +5,9 @@ from fastapi import APIRouter
 from sentinelprobe.core.config import get_settings
 from sentinelprobe.reporting.api import router as reports_router
 from sentinelprobe.vulnerability_scanner.api import router as vulnerability_router
+from sentinelprobe.vulnerability_scanner.attack_pattern_api import (
+    router as attack_pattern_router,
+)
 
 settings = get_settings()
 
@@ -14,6 +17,7 @@ api_router = APIRouter(prefix=settings.API_PREFIX)
 # Include all module routers
 api_router.include_router(reports_router)
 api_router.include_router(vulnerability_router)
+api_router.include_router(attack_pattern_router)
 
 # Add more routers as they are implemented
 # api_router.include_router(reconnaissance_router)
