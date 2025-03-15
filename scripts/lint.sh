404b87a907ea76d6947500f4087f6ac767af0e90
Viraj Kothalawala <vj@heydoor.com.au>
2025-03-15 23:26:51 +1100

    Fix(lint): add D202 to ignored rules for test files

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,7 +7,7 @@ flake8 sentinelprobe/ --config=setup.cfg
 
 # Run flake8 on tests with specific ignores
 echo "Running flake8 on tests with specific ignores..."
-flake8 tests/ --ignore=F401,D102,D105,D107,D400,F811,E501 --config=setup.cfg
+flake8 tests/ --ignore=F401,D102,D105,D107,D400,F811,E501,D202 --config=setup.cfg
 
 # If we get here, it means flake8 didn't find any errors
 echo "✅ No lint errors found!"
