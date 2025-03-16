94b2ea7c891cc7f159d96e80565a329bce329f09
Viraj Kothalawala <vj@heydoor.com.au>
2025-03-16 19:04:36 +1100

    Feat(exploitation): add command injection plugin and fix type issues

--- sentinelprobe/api/router.py
+++ sentinelprobe/api/router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from sentinelprobe.ai_decision.api import router as ai_decision_router
 from sentinelprobe.core.config import get_settings
+from sentinelprobe.exploitation.api import router as exploitation_router
 from sentinelprobe.reporting.api import router as reports_router
 from sentinelprobe.vulnerability_scanner.api import router as vulnerability_router
 from sentinelprobe.vulnerability_scanner.attack_pattern_api import (
@@ -20,6 +21,7 @@ api_router.include_router(reports_router)
 api_router.include_router(vulnerability_router)
 api_router.include_router(attack_pattern_router)
 api_router.include_router(ai_decision_router)
+api_router.include_router(exploitation_router)
 
 # Add more routers as they are implemented
 # api_router.include_router(reconnaissance_router)
